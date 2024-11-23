999d88b99a55f8742b5aa3d355096cd2ae09b91b
Ramtin Imani <ramtinimani.dev@gmail.com>
2024-11-23 23:24:11 +0330

    added mobile header and sidebar and cart

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,14 @@
 //! VARIABLES:
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+const sidebarToggleBtn = document.querySelector(".sidebar-toggle-icon");
+const sidebarCloseBtn = document.querySelector(".sidebar-close-icon");
+const sidebarMenu = document.querySelector(".sidebar-menu");
+const overlay = document.querySelector(".overlay");
+const sidebarDropdown = document.querySelector(".dropdown");
+const sidebarDropdownBtn = document.querySelector(".sidebar-dropdown-btn");
+const shoppingCartToggleBtn = document.querySelector(".shopping-cart-toggle-icon");
+const cartCloseBtn = document.querySelector(".cart-close-icon");
+const shoppingCartSidebar = document.querySelector(".shopping-cart-sidebar");
 
 //! THEME:
 toggleThemeBtns.forEach((btn) => {
@@ -13,3 +22,57 @@ toggleThemeBtns.forEach((btn) => {
     }
   });
 });
+
+//! FUNCTIONS:
+function showSidebarMenu() {
+  sidebarMenu.classList.remove("-right-64");
+  sidebarMenu.classList.add("right-0");
+
+  overlay.classList.add("overlay--visible");
+}
+
+function hideSidebarMenu() {
+  sidebarMenu.classList.remove("right-0");
+  sidebarMenu.classList.add("-right-64");
+
+  overlay.classList.remove("overlay--visible");
+}
+
+function showShoppingCart() {
+  shoppingCartSidebar.classList.remove("-left-64");
+  shoppingCartSidebar.classList.add("left-0");
+
+  overlay.classList.add("overlay--visible");
+}
+
+function hideShoppingCart() {
+  shoppingCartSidebar.classList.remove("left-0");
+  shoppingCartSidebar.classList.add("-left-64");
+
+  overlay.classList.remove("overlay--visible");
+}
+//! EVENTS:
+//! DISPLAY SIDEBAR:
+sidebarToggleBtn.addEventListener("click", showSidebarMenu);
+
+//! CLOSE SIDEBAR:
+sidebarCloseBtn.addEventListener("click", hideSidebarMenu);
+
+//! SIDEBAR DROPDOWN MENU:
+sidebarDropdownBtn.addEventListener("click", (event) => {
+  const dropdownListItem = event.currentTarget.parentElement;
+
+  dropdownListItem.classList.toggle("text-orange-300");
+  sidebarDropdownBtn.classList.toggle("rotate-180");
+
+  sidebarDropdown.classList.toggle("dropdown--open");
+});
+
+//! DISPLAY SHOPPING CART:
+shoppingCartToggleBtn.addEventListener("click", showShoppingCart);
+//! CLOSE SHOPPING CART:
+cartCloseBtn.addEventListener("click", hideShoppingCart);
+
+//! CLOSE SIDEBARS BY OVERLAY:
+overlay.addEventListener("click", hideSidebarMenu);
+overlay.addEventListener("click", hideShoppingCart);
